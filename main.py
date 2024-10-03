9d8de2804c50d94addd7bc26608868a446cb9f0f
Idan Haviv <haviv1idan@gmail.com>
2024-10-03 19:44:54 +0300

    add pre-commit and fix

--- main.py
+++ main.py
@@ -5,20 +5,25 @@ from spotipy.oauth2 import SpotifyOAuth
 
 
 def read_client_data() -> dict[str, str]:
-    with open('conf.yml', 'r') as f:
+    with open("conf.yml", "r") as f:
         data = yaml.safe_load(f)
         return data
 
+
 client_data: dict[str, str] = read_client_data()
-CLIENT_ID: Final[str] = client_data.get('client_id')
-CLIENT_SECRET: Final[str] = client_data.get('client_secret')
-REDIRECT_URI: Final[str] = client_data.get('redirect_uri')
+CLIENT_ID: Final[str] = client_data.get("client_id")
+CLIENT_SECRET: Final[str] = client_data.get("client_secret")
+REDIRECT_URI: Final[str] = client_data.get("redirect_uri")
 
 # Authenticate with Spotify using OAuth
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                               client_secret=CLIENT_SECRET,
-                                               redirect_uri=REDIRECT_URI,
-                                               scope="user-library-read"))
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope="user-library-read",
+    )
+)
 
 # results = sp.current_user_saved_tracks()
 # for idx, item in enumerate(results['items']):
--- saved_tracks.py
+++ saved_tracks.py
@@ -6,30 +6,36 @@ from functools import lru_cache
 
 
 def read_client_data() -> dict[str, str]:
-    with open('conf.yml', 'r') as f:
+    with open("conf.yml", "r") as f:
         data = safe_load(f)
         return data
-    
+
+
 client_data: dict[str, str] = read_client_data()
-CLIENT_ID: Final[str] = client_data.get('client_id')
-CLIENT_SECRET: Final[str] = client_data.get('client_secret')
-REDIRECT_URI: Final[str] = client_data.get('redirect_uri')
+CLIENT_ID: Final[str] = client_data.get("client_id")
+CLIENT_SECRET: Final[str] = client_data.get("client_secret")
+REDIRECT_URI: Final[str] = client_data.get("redirect_uri")
 
 # Authenticate with Spotify using OAuth
-sp = Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                       client_secret=CLIENT_SECRET,
-                                       redirect_uri=REDIRECT_URI,
-                                       scope="user-library-read"))
+sp = Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope="user-library-read",
+    )
+)
+
 
 @lru_cache(maxsize=1)
 def get_saved_tracks():
     saved_tracks = []
     tracks = sp.current_user_saved_tracks()
     while tracks:
-        for i, item in enumerate(tracks['items']):
-           saved_tracks.append(item)
+        for i, item in enumerate(tracks["items"]):
+            saved_tracks.append(item)
 
-        if tracks['next']:
+        if tracks["next"]:
             tracks = sp.next(tracks)
         else:
             tracks = None
@@ -38,6 +44,6 @@ def get_saved_tracks():
 
 saved_tracks = get_saved_tracks()
 for idx, track in enumerate(saved_tracks):
-    name = track['track']['name']
-    artist = track['track']['artists'][0]['name']
+    name = track["track"]["name"]
+    artist = track["track"]["artists"][0]["name"]
     print(f"{idx}. {artist} - {name}", end="\n")
