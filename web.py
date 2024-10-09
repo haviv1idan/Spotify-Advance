5ed3c84b7e4e004441c787dd3d83b011b83dd7f0
haviv1idan <haviv1idan@gmail.com>
2024-10-09 16:42:02 +0000

    change profile app function

--- web.py
+++ web.py
@@ -7,21 +7,9 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def profile():
     user_data = current_user_data()
-    profile_data = f"""
-    <h1>Display your Spotify profile data</h1>
-
-    <section id="profile">
-    <h2>Logged in as <span id="displayName">{user_data['display_name']}</span></h2>
-    <ul>
-        <li>User ID: <span id="id">{user_data['id']}</span></li>
-        <li>Spotify URI: <a id="uri" href="#">{user_data['uri']}</a></li>
-        <li>Spotify Email: <a id="email" href="#">{user_data['email']}</a></li>
-    </ul>
-    </section>
-    """
-    return profile_data
+    return render_template('profile.html', user_data=user_data)
 
 
 @app.route("/top_artists")
@@ -37,10 +25,10 @@ def top_artists():
 @app.route("/get_cache")
 def get_cache():
     print_func_cache()
-    return redirect(url_for("hello_world"))
+    return redirect(url_for("profile"))
 
 
 @app.route("/clear_cache")
 def clear_cache():
     clear_func_cache()
-    return redirect(url_for("hello_world"))
+    return redirect(url_for("profile"))
